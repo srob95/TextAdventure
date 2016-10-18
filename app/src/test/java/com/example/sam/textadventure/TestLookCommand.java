df293fe076d6aec40c43cd4baaa9d27fed70816a
obifang <obifang@gmail.com>
2016-10-18 18:25:51 +1100

    Added a few more tests for LookCommand

--- app/src/test/java/com/example/sam/textadventure/TestLookCommand.java
+++ app/src/test/java/com/example/sam/textadventure/TestLookCommand.java
@@ -57,10 +57,83 @@ public class TestLookCommand {
 
         //Sets the test value to actual
         String actual = look.Execute (test, new String[] { "look", "at", "gem"});
-        //String actual = String.valueOf(test.getInventory().HasItem("Gem"));
-        //String actual = test.getInventory().Fetch("gem").Name();
         //Sets the value of the expected result
         String expected = "A very shiny Gem";
+
+        assertEquals (expected, actual);
+    }
+    @Test
+    public void LookAtNoGemTest() {
+        //Creating an Inventory and Item Object
+        Player test = new Player ("Bob", "Bob's the man");
+        Location location = new Location ("Location", "A location");
+        //Creates a look command
+        LookCommand look = new LookCommand ();
+        Item item = new Item (new String[] {"Gem Stone"}, "Gem", "A very shiny Gem");
+
+        location.addPlayer (test);
+
+        //Sets the test value to actual
+        String actual = look.Execute (test, new String[] { "look", "at", "gem"});
+        //Sets the value of the expected result
+        String expected = "I cannot find the gem";
+
+        assertEquals (expected, actual);
+    }
+    @Test
+    public void LookAtGemInMeTest() {
+        //Creating an Inventory and Item Object
+        Player test = new Player ("Bob", "Bob's the man");
+        Location location = new Location ("Location", "A location");
+        //Creates a look command
+        LookCommand look = new LookCommand ();
+        Item item = new Item (new String[] {"Gem Stone"}, "Gem", "A very shiny Gem");
+
+        test.getInventory().Put (item);
+        location.addPlayer (test);
+
+        //Sets the test value to actual
+        String actual = look.Execute (test, new String[] { "look", "at", "gem", "in", "me"});
+        //Sets the value of the expected result
+        String expected = "A very shiny Gem";
+
+        assertEquals (expected, actual);
+    }
+    @Test
+    public void LookInvalidNoWordsTest() {
+        //Creating an Inventory and Item Object
+        Player test = new Player ("Bob", "Bob's the man");
+        Location location = new Location ("Location", "A location");
+        //Creates a look command
+        LookCommand look = new LookCommand ();
+        Item item = new Item (new String[] {"Gem Stone"}, "Gem", "A very shiny Gem");
+
+        test.getInventory().Put (item);
+        location.addPlayer (test);
+
+        //Sets the test value to actual
+        String actual = look.Execute (test, new String[] { "look", "at", "gem", "in", "me", "gem"});
+        //Sets the value of the expected result
+        String expected = "I don't know how to look like that";
+
+        assertEquals (expected, actual);
+    }
+    @Test
+    public void LookInvalidLookTest() {
+        //Creating an Inventory and Item Object
+        Player test = new Player ("Bob", "Bob's the man");
+        Location location = new Location ("Location", "A location");
+        //Creates a look command
+        LookCommand look = new LookCommand ();
+        Item item = new Item (new String[] {"Gem Stone"}, "Gem", "A very shiny Gem");
+
+        test.getInventory().Put (item);
+        location.addPlayer (test);
+
+        //Sets the test value to actual
+        String actual = look.Execute (test, new String[] { "lookkk", "at", "gem", "in", "me"});
+        //Sets the value of the expected result
+        String expected = "Error in look input";
 
         assertEquals (expected, actual);
     }
